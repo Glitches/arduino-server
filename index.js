50376bb684c1ef2f9e2bcc4e1759c12d46990fbc
Andrea Ceccarelli <gltch2003@gmail.com>
2018-03-06 18:50:15 +0100

    Feature: add prettier and first two routes

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+require('dotenv');
 const express = require('express');
 const app = express();
-const five = require("johnny-five");
+const five = require('johnny-five');
 const board = new five.Board();
+const Port = process.env.PORT || 5000;
+let led;
 
-const port = process.env || 5000 ;
-
-board.on("ready", () => {
+board.on('ready', () => {
   // Create an Led on pin 13
-  var led = new five.Led(13);
+  led = new five.Led(13);
 });
 
 app
-  .get('/', (req,res) => {
-  // Strobe the pin on/off, defaults to 100ms phases
-  led.strobe();
+  .get('/', (req, res) => {
+    // Strobe the pin on/off, defaults to 100ms phases
+    led.strobe();
+    res.send('Hello World!');
   })
-  .listen(port, () => console.log(`Server running on port ${port}`));
+  .get('/off', (req, res) => {
+    // Turn off the strobe
+    led.stop();
+    res.status(200).end();
+  })
+  .listen(Port, () => console.log('Server running on port ', Port));
