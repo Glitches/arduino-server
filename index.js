10bd4d57adc2553ea00d904e9b0898a732765154
Andrea Ceccarelli <gltch2003@gmail.com>
2018-03-07 12:01:09 +0100

    Feature: add try/catch and fade route

--- index.js
+++ index.js
@@ -12,14 +12,39 @@ board.on('ready', () => {
 });
 
 app
-  .get('/', (req, res) => {
+  .get('/', async (req, res) => {
     // Strobe the pin on/off, defaults to 100ms phases
-    led.strobe();
-    res.send('Hello World!');
+    const strobe = new Promise((resolve, reject) => {
+      try {
+        led.strobe();
+        resolve(200);
+      } catch (e) {
+        reject(400);
+      }
+    });
+    try {
+      const val = await strobe;
+      res.sendStatus(val);
+    } catch (e) {
+      console.log(e);
+    }
   })
   .get('/off', (req, res) => {
     // Turn off the strobe
     led.stop();
+    res.status(200).end();
+  })
+  .get('/fade', (req, res) => {
+    // Light fader
+    led.fade({
+      easing: 'outSine',
+      duration: 1000,
+      cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
+      keyFrames: [0, 250, 25, 150, 100, 125],
+      onstop: function() {
+        console.log('Animation stopped');
+      }
+    });
     res.status(200).end();
   })
   .listen(Port, () => console.log('Server running on port ', Port));
