cdbafec3a542abda0e6b587119b15c143e1c07fc
Andrea Ceccarelli <gltch2003@gmail.com>
2018-03-11 17:21:27 +0100

    Feature: add basic socket.io implemementation

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ require('dotenv');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const server = require('http').createServer(app);
 const router = require('./router');
 const Port = process.env.PORT || 5000;
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('message', () => {});
+  socket.on('disconnect', () => {});
+});
 
 app
   .use(cors())
   .use(router)
   .listen(Port, () =>
+    // eslint-disable-next-line no-console
     console.log(`==============================\n
 Server running on port: ${Port} \n
 ==============================\n`)
